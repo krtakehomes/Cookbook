588f0131e18c462bb27e0f7eedbf417fb18e12fd
Kyle Ronayne <kyle.ronayne@icloud.com>
2024-10-03 21:27:23 -0400

    Initial Commit

---
+++ Cookbook/CookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookbookApp.swift
+//  Cookbook
+//
+//  Created by Kyle Ronayne on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct CookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
